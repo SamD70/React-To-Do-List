e6a417113ec80574463f4a9e86ff25f9b9956385
Sam Dolby <sam@dolby.org.uk>
2021-12-04 16:14:34 +0000

    app finished

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -1,26 +1,69 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 //Importing Components
 import Form from "./components/Form";
 import TodoList from "./components/To-do list";
 
 function App() {
+  
+  //States
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  //Run once when the app starts
+  useEffect(() =>{
+    getLocalTodos();
+  },[])
+  //Use Effect
+  useEffect(() => {
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, status]);
+  //Functions
+  const filterHandler = () =>{
+    switch(status){
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+  //Save to local storage
+  const saveLocalTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  };
+  const getLocalTodos = () =>{
+    if(localStorage.getItem('todos') === 'null'){
+      localStorage.setItem('todos', JSON.stringify([]));
+    }
+    else{
+      let todoLocal = JSON.parse(localStorage.getItem('todos'));
+      setTodos(todoLocal);
+    }
+  };
   return (
     <div className="App">
       <header>
-        <h1>Sam's To-Do list</h1>
+        <h1>To-Do list</h1>
       </header>
       <Form 
         inputText={inputText} 
         todos={todos} 
         setTodos={setTodos} 
         setInputText={setInputText}
+        setStatus={setStatus}
+        
       />
       <TodoList 
       setTodos={setTodos}
       todos={todos}
+      filteredTodos={filteredTodos}
       />
     </div>
   );
